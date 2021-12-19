4c9c40ef87be1c5fe71f66dd8f91ec9827ff4b98
Sandi Irawan <sandicdma@gmail.com>
2021-12-19 13:30:10 +0700

    your commit message

---
+++ dist/reset/reset.service.d.ts
@@ -0,0 +1,9 @@
+import { ResetEntity } from "./reset.entity";
+import { Repository } from "typeorm";
+import { Reset } from "./reset.interface";
+export declare class ResetService {
+    private readonly resetRepository;
+    constructor(resetRepository: Repository<ResetEntity>);
+    create(reset: Reset): Promise<Reset>;
+    findOne(condition: any): Promise<Reset>;
+}
